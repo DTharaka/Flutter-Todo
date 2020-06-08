cab77f54200841c19ea77bdfae750f8ddc5250ef
DTharaka <tharakadil22@gmail.com>
2020-06-08 21:32:24 +0530

    add some features

--- lib/models/todo_for_listing.dart
+++ lib/models/todo_for_listing.dart
@@ -1,9 +1,8 @@
 class TodoForListing {
   String todoID;
   String todoTitle;
-  DateTime createDateTime;
-  DateTime lastEditedDateTime;
+  bool isCompleted;
 
-  TodoForListing({this.todoID, this.todoTitle, this.createDateTime, this.lastEditedDateTime});
+  TodoForListing({this.todoID, this.todoTitle, this.isCompleted});
 
 }
--- lib/services/todo_service.dart
+++ lib/services/todo_service.dart
@@ -1,27 +1,30 @@
+import 'dart:convert';
+
+import 'package:todo_app/models/api_response.dart';
 import 'package:todo_app/models/todo_for_listing.dart';
+import 'package:http/http.dart' as http;
 
 class TodoServices{
-  List<TodoForListing> getTodoList(){
-    return [
-    new TodoForListing(
-      todoID: '1',
-      todoTitle: 'Todo 1', 
-      createDateTime: DateTime.now(),
-      lastEditedDateTime: DateTime.now()
-    ),
-    new TodoForListing(
-      todoID: '2',
-      todoTitle: 'Todo 2', 
-      createDateTime: DateTime.now(),
-      lastEditedDateTime: DateTime.now()
-    ),
-    new TodoForListing(
-      todoID: '3',
-      todoTitle: 'Todo 3', 
-      createDateTime: DateTime.now(),
-      lastEditedDateTime: DateTime.now()
-    )
-  ];
-  }
 
+  String apiKey = 'https://jsonplaceholder.typicode.com/todos/1';
+
+  Future <ApiResponse<List<TodoForListing>>> getTodoList(){
+      return http.get(apiKey).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = jsonDecode(data.body);
+        print(jsonData);
+        final todos = <TodoForListing>[];
+        for (var item in jsonData) {
+          final todo = TodoForListing(
+            todoID: item['id'],
+            todoTitle: item['title'],
+            isCompleted:  item['completed']
+          );
+          todos.add(todo);
+        }
+        return ApiResponse<List<TodoForListing>>(data: todos);
+      }
+      return ApiResponse<List<TodoForListing>>(error: true, errorMessage: 'An error occured');
+    }).catchError((_)=> ApiResponse<List<TodoForListing>>(error: true, errorMessage: 'An error occured'));
+  }
 }
